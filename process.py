723a5070a95c17954b37cc2af2a66628757911ae
mira <ilmirashaim@gmail.com>
2016-07-27 11:20:38 +0300

    renaming and min/max length count

--- process.py
+++ process.py
@@ -1,48 +1,51 @@
 import csv
 import os
-import matplotlib.pyplot as plt
+
 dataFolder = 'data'
 outFolder = 'out'
 keys = ['A', 'W', 'I', 'Y', 'G', 'H', 'D', 'F', 'C', 'P', 'Q', 'M', 'V', 'S', 'E', 'K', 'R', 'L', 'N', 'T']
-dicts = []
+aminoAcidFreqDicts = []
 
-
-
+lengths = []
 filenames = ['azh.txt', 'yzh.txt', 'gs.txt', 'kb.txt', 'tv.txt', 'irzv.txt']
 
 for filename in filenames:
-   d = dict.fromkeys(keys,0)
+   aminoAcidFreqDict = dict.fromkeys(keys,0)
 
    with open(os.path.join(dataFolder, filename), 'r') as csvfile:
       reader = csv.reader(csvfile, delimiter='\t', )
       next(reader)
-      sum_of_umicount=0.0
+      sumOfUmicount=0.0
 
       props = []
       for row in reader:
-         sum_of_umicount += float(row[1])
+         sumOfUmicount += float(row[1])
          seq = row[5]
 
          prop = int(row[0])
          if prop == 0:
             continue
-            
+
          if prop < 100:
             props.append(prop)
 
-         if seq[0] != 'C':
-            print('Achtung')
          if '*' in seq or '~' in seq:
             continue
          for s in seq:
-            d[s] = d[s] + 1
+            aminoAcidFreqDict[s] = aminoAcidFreqDict[s] + 1
+
+         lengths.append(len(seq))
 
       # plot of Umo.count
-      plt.figure()
-      plt.plot(props)
+      # plt.figure()
+      # plt.plot(props)
 
-   dicts.append(d)
-plt.show()
+   aminoAcidFreqDicts.append(aminoAcidFreqDict)
+# plt.show()
+
+print(min(lengths))
+print(max(lengths))
+
 
 
 # ouput of frequences of each amino acid
@@ -53,7 +56,7 @@ with open(os.path.join(outFolder, freqOut), 'w') as freqOutFile:
       freqsForFile = []
       freqOutFile.write("\n")
       for k in keys:
-         freqsForFile.append(dicts[i][k])
+         freqsForFile.append(aminoAcidFreqDicts[i][k])
       sumOfFreqs = sum(freqsForFile)
       freqOutFile.write(', '.join([str(round(x/float(sumOfFreqs),4)) for x in freqsForFile]))
 
