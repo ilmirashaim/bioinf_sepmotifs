56e20d71d0b481ee283bc114baee9338e322be6f
mira <ilmirashaim@gmail.com>
2016-07-29 15:21:03 +0300

    clustering for twins peptides

--- kideraFactors.R
+++ kideraFactors.R
@@ -1,6 +1,6 @@
 library(Peptides)
-n=100
-data <- read.csv('data/azh_1.txt', sep='\t', nrows= n,
+#n=100
+data <- read.csv('data/comp/irzv_1-tv_2_twins.csv', sep='\t',
                  stringsAsFactors = FALSE)
 
 getKideraFactors = function(seq){
@@ -11,12 +11,13 @@ getKideraFactors = function(seq){
             "pK.C", "surrounding.hydrop"), kidera, seq = seq)
     x
 }
-seqs <- data$CDR3.amino.acid.sequence[1:n]
+seqs <- data$CDR3.amino.acid.sequence
 resMat <- sapply(seqs, getKideraFactors)
 resMat <- t(resMat)
 res <- data.frame(resMat)
 res$seq <- as.character(seqs)
-#write.table(res[,c(11,1:10)], file='data/kidera/azh_1.txt', sep="\t", quote=FALSE)
+write.table(res[,c(11,1:10)], file='data/kidera/irzv_1-tv_2_twins.csv', sep="\t", quote=FALSE)
 
-hc <- hclust(dist(resMat), "ave", labels = as.character(seqs))
-plot(hc)
+hc <- hclust(dist(resMat), "ave")
+plot(hc, labels = as.character(seqs))
+groups <- cutree(hc,3)
